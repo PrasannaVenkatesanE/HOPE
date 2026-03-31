a4bcaa382edb26d69524f418a107b70ac8d63487
Prasanna Venkatesan E <prasanna.108411913@gmail.com>
2026-03-31 09:52:38 +0530

    new change

--- Day-7/inheritance.java
+++ Day-7/inheritance.java
@@ -1,12 +1,18 @@
-public class Animal{
-
+class Animal{
+    void display(){
+        System.out.println("There are multiple Animals Choose 1 and mention it's sound:");
+    }
 }
-
-
 class Dog extends Animal{
     void bark(){
-        System.out.println("Since you are a dog barks!!!!!!!!!!!!")
+        System.out.println("Dog barks");
     }
 }
 
-class 
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog sound=new Dog();
+        sound.bark();
+        System.out.println(sound.hashCode());
+    }
+}
